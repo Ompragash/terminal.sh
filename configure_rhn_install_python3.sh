38e6ee07018c4f298e6739c3f714dd01d199f29f
Ompragash Viswanathan <ompragash.viswanathan@gmail.com>
2021-04-06 10:57:33 +0530

    Create configure_rhn_install_python3.sh

---
+++ configure_rhn_install_python3.sh
@@ -0,0 +1,12 @@
+subscription-manager register --username=rhn-support-oviswana --password='##21221544bB$$'
+
+sudo subscription-manager repos --enable rhel-7-server-optional-rpms --enable rhel-server-rhscl-7-rpms
+
+sudo yum -y install @development
+
+sudo yum -y install rh-python36
+
+sudo yum -y install rh-python36-numpy \
+ rh-python36-scipy \
+ rh-python36-python-tools \
+ rh-python36-python-six
