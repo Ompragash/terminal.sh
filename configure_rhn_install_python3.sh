dc4587933a41f685ef9a58b904b51074ff7cecdf
Ompragash Viswanathan <ompragash.viswanathan@gmail.com>
2021-04-06 11:07:51 +0530

    Update configure_rhn_install_python3.sh

--- configure_rhn_install_python3.sh
+++ configure_rhn_install_python3.sh
@@ -6,7 +6,4 @@ sudo yum -y install @development
 
 sudo yum -y install rh-python36
 
-sudo yum -y install rh-python36-numpy \
- rh-python36-scipy \
- rh-python36-python-tools \
- rh-python36-python-six
+sudo yum -y install rh-python36-numpy rh-python36-scipy rh-python36-python-tools rh-python36-python-six
