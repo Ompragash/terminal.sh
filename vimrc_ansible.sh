0a1aabf7dd95d0fcb4786da87a4a2a6d064df7e0
Ompragash Viswanathan <ompragash.viswanathan@gmail.com>
2022-02-09 21:58:13 +0530

    Update and rename vimrc_ansible to vimrc_ansible.sh

--- vimrc_ansible.sh
+++ vimrc_ansible.sh
@@ -1,3 +1,8 @@
+#!/bin/bash
+#
+# Configure .vimrc for YAML and Ansible.
+
+cat > ~/.vimrc <<EOF
 autocmd FileType yaml setlocal ts=2 sts=2 sw=2
 set et ai sw=2 ts=2 tw=78 sts=2 cuc
 set cursorline
@@ -5,3 +10,4 @@ set cursorcolumn
 set hidden
 set autoindent
 set smartindent
+EOF
