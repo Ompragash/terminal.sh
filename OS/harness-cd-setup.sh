8e3607222c88ef572dc67792c793c03cc60305a8
Ompragash Viswanathan <ompragash@proton.me>
2023-01-20 08:09:38 +0530

    Updated OS/harness-cd-setup.sh
    Signed-off-by: Ompragash Viswanathan <ompragash@proton.me>

--- OS/harness-cd-setup.sh
+++ OS/harness-cd-setup.sh
@@ -35,6 +35,7 @@ install_docker_ubuntu() {
     echo   "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
     $(lsb_release -cs) stable" | tee /etc/apt/sources.list.d/docker.list > /dev/null
     chmod a+r /etc/apt/keyrings/docker.gpg
+    apt-get update
     apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin -y
     systemctl enable docker; systemctl start docker
 }
